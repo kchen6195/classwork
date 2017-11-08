826eb48a6b4fe0fc4712aa1af76bd4f06a8cd027
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-08 13:00:24 -0500

    print

--- ArraysTheHardWay/src/CopyArrays.java
+++ ArraysTheHardWay/src/CopyArrays.java
@@ -35,7 +35,7 @@ public class CopyArrays {
 	{
 		for(int i= 0; i < arr.length; i++)
 		{
-			System.out.println("[" + arr[i]+ "]");
+			System.out.print("[" + arr[i]+ "]");
 			
 		}
 		System.out.println();
