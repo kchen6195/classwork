b61ba7e00e1f3387924bd73581f06b9b00a17ec4
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-12-13 13:11:26 -0500

    ListRemoveDups

---
+++ ArraysTheHardWay/src/ListRemoveDups.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.List;
+
+public class ListRemoveDups {
+
+	public static void main(String[] args) {
+		
+	}
+	public static List<Integer> RemoveDuplicates(List<Integer> nums)
+	{
+		Collections.sort(nums);
+		for(int x = 0; x < nums.size()-1;x++)
+		{
+			for(int y = x+1; y < nums.size();y++)
+			{
+				if()
+				{
+					
+				}
+			}
+		}
+		
+	}
+}
